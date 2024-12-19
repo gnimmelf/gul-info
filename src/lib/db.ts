ebf9f096ee2b88bf9d99e57a0c0b4f5573c7403d
Flemming Hansen <gnimmelf@gmail.com>
2024-12-19 13:20:54 +0100

    Updated Db class

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,31 +1,51 @@
-import Surreal from "surrealdb";
+import Surreal, { surql } from "surrealdb";
+import { awaitCondition } from "./utils";
 
-// Define the database configuration interface
 interface DbConfig {
-  url: string;
-  namespace: string;
-  database: string;
+  namespace: string
+  database: string
+  url: string
 }
 
-// Define the default database configuration
-const DEFAULT_CONFIG: DbConfig = {
-  url: "http://127.0.0.1:8000/rpc",
-  namespace: "intergate",
-  database: "gul-info",
-};
+export class Db {
+  private static instance: Db;
 
-// Define the function to get the database instance
-export async function getDb(config: DbConfig = DEFAULT_CONFIG): Promise<Surreal> {
-  const db = new Surreal();
+  private db = new Surreal()
+  private config: DbConfig
 
-  try {
-    await db.connect(config.url);
-    await db.use({ namespace: config.namespace, database: config.database });
-    return db;
-  } catch (err) {
-    console.error("Failed to connect to SurrealDB:", err instanceof Error ? err.message : String(err));
-    await db.close();
-    throw err;
+  constructor(config: Partial<DbConfig> = {}) {
+    this.config = Object.assign({
+      url: "https://127.0.0.1:7999/rpc",
+      namespace: "intergate",
+      database: "gul-info",
+    }, config)    
+  }
+
+  public static getInstance(): Db {
+    if (!Db.instance) {
+      Db.instance = new Db();
+    }
+    return Db.instance;
+  }
+
+  async connect() {
+    if (!this.db.ready) {
+      try {
+        const { namespace, database } = this.config
+        await this.db.connect(this.config.url, { namespace, database });
+        await this.db.ready        
+      } catch (err) {
+        console.error("Failed to connect to SurrealDB:", err instanceof Error ? err.message : String(err));
+        await this.db.close();
+        throw err;
+      }
+    }    
+    return 'connected'
+  }
+
+  async getListings() {
+    await this.connect()
+    const query = surql`SELECT * FROM Test;`
+    return (await this.db.query(query)).pop()
   }
 }
-
